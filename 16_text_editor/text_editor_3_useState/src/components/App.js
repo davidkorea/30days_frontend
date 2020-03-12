310ebcf2960f2b6b35b71e02e06aa6a53650250b
davidkorea <lyfirst@naver.com>
2020-03-12 22:36:17 +0800

    Add files via upload

---
+++ 16_text_editor/text_editor_3_useState/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Editor from './Editor'
+
+function App() {
+    return (
+        <div className='flex items-center justify-center h-screen bg-gray-100'>
+            <Editor></Editor>
+        </div>
+    )
+}
+export default App
