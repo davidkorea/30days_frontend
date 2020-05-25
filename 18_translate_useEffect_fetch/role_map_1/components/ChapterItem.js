3abbf61a256d46fbf11cc38986ff013c6e93a7ca
davidkorea <lyfirst@naver.com>
2020-05-25 14:21:10 +0900

    Add files via upload

--- 18_translate_useEffect_fetch/role_map_1/components/ChapterItem.js
+++ 18_translate_useEffect_fetch/role_map_1/components/ChapterItem.js
@@ -3,8 +3,9 @@ import React from 'react';
 function ChapterItem(props){
     return(
         
-        <div className="item h-4 w-6 m-1 rounded bg-gray-700 text-white text-xs 
-            flex justify-center items-center cursor-pointer"
+        <div className={`item h-4 w-6 m-1 rounded bg-gray-700 text-xs 
+            flex justify-center items-center cursor-pointer
+            ${props.selected_id==props.chapter_id? 'text-yellow-400 text-sm': 'text-white'} `}
             onClick={()=>props.handleChapter(props.chapter_id)}
         >
             {props.chapter_id}
